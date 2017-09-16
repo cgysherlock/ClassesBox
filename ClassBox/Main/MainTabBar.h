a9f1e442a3483bc341f85c0f81d5d085310d2cce
Wrappers <zhanglive@outlook.com>
2017-09-16 09:37:57 +0800

    完成tabBar

---
+++ ClassBox/Main/MainTabBar.h
@@ -0,0 +1,14 @@
+//
+//  MainTabBar.h
+//  ClassBox
+//
+//  Created by Wrappers Zhang on 2017/9/16.
+//  Copyright © 2017 sherlock. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface MainTabBar : UITabBar 
+
+@end
