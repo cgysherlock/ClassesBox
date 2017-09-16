d42b6cb60fdeb8e3c2281ad09688efb5497c5041
Wrappers <zhanglive@outlook.com>
2017-09-16 10:52:23 +0800

    add

---
+++ ClassBox/ClassBox/Publish/Controller/PublishViewController.h
@@ -0,0 +1,15 @@
+//
+//  PublishViewController.h
+//  ClassBox
+//
+//  Created by Wrappers Zhang on 2017/9/16.
+//  Copyright © 2017 sherlock. All rights reserved.
+//
+
+
+
+#import "BaseViewController.h"
+
+@interface PublishViewController : BaseViewController
+
+@end
---
+++ ClassBox/ClassBox/Publish/Controller/PublishViewController.m
@@ -0,0 +1,23 @@
+//
+//  PublishViewController.m
+//  ClassBox
+//
+//  Created by Wrappers Zhang on 2017/9/16.
+//  Copyright © 2017 sherlock. All rights reserved.
+//
+
+#import "PublishViewController.h"
+
+@implementation PublishViewController
+
+#pragma mark - 生命周期
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+#pragma mark - 初始化
+- (void)setUpView {
+
+}
+@end
